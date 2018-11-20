475965a0af63f9c33f9b5df7ccacec887862822d
Alex <alexcannon2018@gmail.com>
2018-11-20 10:52:22 -0700

    Added a basic project structure.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './scss/components/index.css';
+import App from './js/components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));

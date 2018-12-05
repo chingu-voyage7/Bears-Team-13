b1af328a88a31338c4ea53d9307c0a9bd82255a0
Alex <alexcannon2018@gmail.com>
2018-12-05 11:23:45 -0700

    quick

--- src/js/server/utils/mailer.js
+++ src/js/server/utils/mailer.js
@@ -1,3 +1,14 @@
+const config = require('../../../config.js');
 const sgMail = require('@sendgrid/mail');
-console.log(process.env.REACT_APP_SG_API_KEY);
-sgMail.setApiKey(process.env.REACT_APP_SG_API_KEY)
+sgMail.setApiKey(config.SG_KEY);
+
+module.exports = {
+  invite: function(to, firstName) {
+    sgMail.send({
+      to,
+      from: "test@example.com",
+      subject: firstName + " invited you to his Secret Santa event.",
+      html: "<p>Click here to join </p>"
+    })
+  }
+}

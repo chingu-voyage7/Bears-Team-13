186a920ec8c3384670b2e77dabdd1947e77e1638
Inna <designbyinna@gmail.com>
2018-12-07 17:14:00 -0700

    button on hover styled

--- src/js/pages/Signup/signup-style.js
+++ src/js/pages/Signup/signup-style.js
@@ -55,13 +55,29 @@ background-size:cover;
 
 const InputSubmit = styled.input`
 /* margin-top:140px; */
-border-radius:10px;
+width:100px;
+border-radius:5px;
+margin:0 auto;
 margin-top:40px;
 padding:10px;
-background-color:#D10A0A;
-border-color:#D10A0A;
-color:white;
+border:1px #D10A0A solid;
+color:#D10A0A;
+font-weight:bold;
 box-shadow:2px 2px 2px rgba(0,0,0,0.5);
+transition:.2s all linear;
+
+ &:hover {
+    background: #D10A0A;
+    color:white;
+  }
+  &:focus{
+      outline:0;
+  }
+  &:active{
+    box-shadow:0px 0px 0px;
+    top:2px;
+
+  }
 
 `
 

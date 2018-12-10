8656daeff8dd57efec96bce6c9c9713f70db7497
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-12-10 17:32:19 -0600

    Add store functionality

--- src/js/pages/Store/Store.js
+++ src/js/pages/Store/Store.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
-
+const Item = ({item}) => {
+  return (
+    <article>
+      <h3>{item.name}</h3>
+      <img src="" alt="img "/>
+      <div>
+        <span>{item.usd}</span>
+      </div>
+    </article>
+  )
+}
 export default class Store extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      items: {},
-      query: ''
+      items: [],
+      query: '',
+      loader: false,
     }
   }
 
@@ -24,40 +35,56 @@ export default class Store extends Component {
     e.preventDefault()
 
     const { query } = this.state
+    const url = `/api/finditem?keywords=${query}`
 
-    const url = `/api/finditem?keywords=Candy+Canes&page=0`
-
-    console.log(url)
+    this.setState({
+      loader: true
+    })
 
     axios.get(url)
       .then(res => {
         console.log(res.data)
+        this.setState({
+          items: res.data,
+          loader: false,
+        })
       })
       .catch(err => {
+        this.setState({
+          loader: false
+        })
         console.log(err.response)
       })
   }
 
   render() {
+    const { items, loader } = this.state
+    let storeItems
+
+    if (items.length > 0) {
+      storeItems = items.map(item => {
+        return (
+          <Item item={item} key={item._id}></Item>
+        )
+      })
+    } else {
+      storeItems = "Items not found";
+    }
+
     return (
       <>
-      <section>
-        <form>
-          <input type="text" onChange={this.handleInputSearch}/>
-          <div style={{ display: 'inline-block' }}>
-            <button>Price Dropdown</button>
-            <button type="submit" onClick={this.handleSearch}>Search</button>
-          </div>
-        </form>
-      </section>
-      <section>
-        <article>
-          <img src="" alt="img "/>
-          <div>
-            <span>Price</span>
-          </div>
-        </article>
-      </section>
+        <section>
+          <form>
+            <input type="text" onChange={this.handleInputSearch}/>
+            <div style={{ display: 'inline-block' }}>
+              <button>Price Dropdown</button>
+              <button type="submit" onClick={this.handleSearch}>Search</button>
+            </div>
+          </form>
+        </section>
+        <section>
+          {storeItems}
+        </section>
       </>
     )
   }

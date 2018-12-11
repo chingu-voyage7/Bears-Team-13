6fc370b1201d88712368591cb67b9d3b7aa69b87
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-12-11 17:02:00 -0600

    Load events and add functionality to go to one event

--- src/js/pages/Dashboard/Dashboard.js
+++ src/js/pages/Dashboard/Dashboard.js
@@ -1,18 +1,81 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
+const Event = ({event, handleEventClick}) => {
+  return (
+    <article
+      onClick={handleEventClick}>
+      <h3>{event.name}</h3>
+      <div>
+        <div>
+          Event start at <span>{event.startDate}</span>
+        </div>
+        <span>author</span>
+      </div>
+    </article>
+  )
+}
+
 export default class Dashboard extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      events: [],
+      loader: false
     }
   }
 
+  handleEventClick(eventId) {
+    const { history } = this.props
+    console.log(`/dashboard/${eventId}`)
+
+    history.push(`/dashboard/${eventId}`)
+  }
+
+  componentDidMount() {
+    this.setState({
+      loader: true
+    })
+    axios.get('/api/myevents')
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          events: res.data,
+          loader: false
+        })
+      })
+      .catch(err => console.log(err.response))
+  }
+
   render() {
+    const { events, loader } = this.state
+    let userEvents
+
+    if (events.length > 0) {
+      userEvents = events.map(event => {
+        return(
+          <Event
+            event={event}
+            key={event._id}
+            handleEventClick={() => this.handleEventClick(event._id)}>
+          </Event>
+        )
+      })
+    } else {
+      userEvents = `You don't have any events yet`;
+    }
+
     return (
-      <div>Dashboard</div>
+      <main>
+        <section>
+          {
+            loader
+              ? 'Loading...'
+              : userEvents
+          }
+        </section>
+      </main>
     )
   }
 }

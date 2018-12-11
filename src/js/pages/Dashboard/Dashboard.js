b9faa912de041120dcf8695e5d0c68aecabdce2f
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-12-11 14:33:19 -0600

    Initial Setup for Dashboard component

---
+++ src/js/pages/Dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>Dashboard</div>
+    )
+  }
+}

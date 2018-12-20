0a29723bb00a998889cab61d0e0de3cf1f1a4667
Alex <alexcannon2018@gmail.com>
2018-12-20 09:59:12 -0700

    Added API /mysecretsantas and FUNCTIONAL endDate

--- src/js/server/api/_user.js
+++ src/js/server/api/_user.js
@@ -35,7 +35,33 @@ router.get('/myuser', isAuth, function (req, res) {
 
 // Returns a list of your secret santa { users }
 router.get("/myrecipients", isAuth, function (req, res) {
-  return res.sendStatus(500);
+
+  // Get user events
+  User.findOne({_id: new ObjectID(req.user._id)}, {events: 1}, (err, user) => {
+    if (err) { return res.sendStatus(500); }
+    if (!user) { return res.sendStatus(404); }
+
+    console.log("EVENTS: " + user.events.length);
+
+    // Get ssList for each event
+    Event.find({_id: {$in: user.events}}, {ssList: 1}, (err, events) => {
+      if (err) { return res.sendStatus(500); }
+      if (!events) { return res.sendStatus(404); }
+
+      // Find my [user_id, recipient_id] pair(s) in the list
+      const recipients = events.map((event) => {
+        for (let i = 0; i < event.ssList.length; i++) {
+          if (ObjectID.toString(event.ssList[i][0]) === ObjectID.toString(req.user._id)) {
+            if (event.ssList[i][1]) {
+              return event.ssList[i][1];
+            }
+          }
+        }
+      });
+
+      return res.json(recipients);
+    });
+  });
 });
 
 // Adds user to the DB

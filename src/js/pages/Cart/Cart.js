dd2fe91403f17c8544e1b72f7375fea7ff79e6e9
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-12-24 21:38:47 -0600

    Add list cartItems

--- src/js/pages/Cart/Cart.js
+++ src/js/pages/Cart/Cart.js
@@ -13,6 +13,7 @@ export default class Cart extends Component {
   fetchCartItems() {
     axios.get('/api/mycart')
       .then( res => {
+        this.setState({cartItems: res.data})
         console.log(res.data)
       })
       .catch(err => console.log(err.response))
@@ -23,35 +24,43 @@ export default class Cart extends Component {
   }
 
   render() {
+    const { cartItems } = this.state
+
     return (
       <section>
         <h1>Carrito</h1>
         { /*Headers*/ }
         <div>
           <div>
-            Precio
+            Price
           </div>
           <div>
-            Cantidad
+            Quantity
           </div>
         </div>
         { /* List items */}
         <div>
-          <article>
-            <img src="" alt="item image" />
-            <div>
-              <h3>Nombre Articulo</h3>
-              <span>$233.99</span>
-              <select>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-              </select>
-            </div>
-          </article>
+          {
+            cartItems.map( cartItem => {
+              return (
+                <article key={cartItem._id}>
+                  <img src="" alt="item image" />
+                  <div>
+                    <h3>{cartItem.name}</h3>
+                    <span>${cartItem.usd}</span>
+                    <select>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                    </select>
+                  </div>
+                </article>
+              )
+            })
+          }
         </div>
       </section>
     )

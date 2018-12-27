c183f06617d07b34054c8903df53f0c189673aa5
Alex <alexcannon2018@gmail.com>
2018-12-27 13:55:37 -0700

    tmp built files

---
+++ build/precache-manifest.aa41eef27dffd9e4426d1687ca303606.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "807ca619934da12d5c6bbfb52c686034",
+    "url": "/static/media/red_ball.807ca619.jpg"
+  },
+  {
+    "revision": "3fc9b4c4d6d66a2a2de7",
+    "url": "/static/css/main.a631e1d2.chunk.css"
+  },
+  {
+    "revision": "5810b70bd58944955831",
+    "url": "/static/js/1.5810b70b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dbabd737f81ab8cdc5d4e2062d5d9df5",
+    "url": "/static/media/gift.dbabd737.jpg"
+  },
+  {
+    "revision": "3fc9b4c4d6d66a2a2de7",
+    "url": "/static/js/main.3fc9b4c4.chunk.js"
+  },
+  {
+    "revision": "33631459cc8635c157758945c295c786",
+    "url": "/static/media/christmas.33631459.jpeg"
+  },
+  {
+    "revision": "b04d572087c0b39cf7815fb3eb4d50cb",
+    "url": "/static/media/gift-profile.b04d5720.jpeg"
+  },
+  {
+    "revision": "1f1fa1de05f9642f1e93b44548269992",
+    "url": "/static/media/pine-cones.1f1fa1de.jpeg"
+  },
+  {
+    "revision": "30b7c6abf6fd2f4791b7964933253f32",
+    "url": "/static/media/invites-2.30b7c6ab.jpeg"
+  },
+  {
+    "revision": "59016598683f2ba36795ebcb1273e196",
+    "url": "/index.html"
+  }
+];

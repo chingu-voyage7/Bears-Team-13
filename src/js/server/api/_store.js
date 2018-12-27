06e71bb58d66b4f65bca1e3fe105495521ad9999
Alex <alexcannon2018@gmail.com>
2018-12-27 13:41:42 -0700

    removed bad code

--- src/js/server/api/_store.js
+++ src/js/server/api/_store.js
@@ -62,11 +62,6 @@ router.post('/additem', isAuth, (req, res) => {
 
 });
 
-const config = require('config.js');
-Object.keys(config).map((key) => {
-  process.env[key] = config[key];
-});
-
 //
 // Cart CRUD
 //

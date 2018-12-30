9f4c3ad76704ec4293964254edcddd59db404018
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-12-30 16:53:54 -0600

    Add purchase page

--- src/js/pages/Payment/Payment.js
+++ src/js/pages/Payment/Payment.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import {CartWrap, ItemsWrap, Item, ImageWrap, ImagePriceWrap, PriceSelectWrap, ItemName, Price, Delete, Checkout} from './cart-style';
-import {Greeting} from '../MyAccount/myAccount-style'
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import axios from "axios";
+import {
+  CartWrap,
+  ItemsWrap,
+  Item,
+  ImageWrap,
+  ImagePriceWrap,
+  PriceSelectWrap,
+  ItemName,
+  Price,
+  Delete,
+  Checkout
+} from "./cart-style";
+import { Greeting } from "../MyAccount/myAccount-style";
+import { Link } from "react-router-dom";
 
 export default class Cart extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      cart: [], // [{event, item}]
-    }
+      cart: [] // [{event, item}]
+    };
   }
 
   componentDidMount() {
@@ -18,31 +29,31 @@ export default class Cart extends Component {
   }
 
   fetchCart() {
-    axios.get('/api/mycart')
-      .then( res => {
+    axios
+      .get("/api/mycart")
+      .then(res => {
         if (Object.keys(res.data).length > 0) {
-          this.setState({cart: res.data});
+          this.setState({ cart: res.data });
         }
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   }
 
-  // Removes {event, item} from cart
-  onDelete(e, event_id) {
-    axios.delete('/api/mycart/delete', { data: {event_id} })
-    .then((res) => alert("Untested delete success"))
-    .catch(err => alert("Untested delete failure"));
+  onCompletePurchase = () => {
+    axios
+      .post("/api/mycart/purchase")
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response));
   }
 
   render() {
-    const {
-      cart,
-    } = this.state;
+    const { cart } = this.state;
     const that = this;
 
-    return <CartWrap>
+    return (
+      <CartWrap>
         <Greeting>My Cart</Greeting>
 
         {/* List items */}
@@ -56,27 +67,30 @@ export default class Cart extends Component {
               const { event, item } = pair;
               console.log(JSON.stringify(item));
 
-              return <Item key={event._id}>
+              return (
+                <Item key={event._id}>
                   <ItemName>{item.name}</ItemName>
                   <ImagePriceWrap>
                     <ImageWrap>
-                      <img src={"/api/static/images/item." + item._id} alt="item" />
+                      <img
+                        src={"/api/static/images/item." + item._id}
+                        alt="item"
+                      />
                     </ImageWrap>
                     <PriceSelectWrap>
                       <Price>${item.usd}</Price>
                       <p>
                         Gift for {event.recipient.username} @ {event.name}
                       </p>
-                      <Delete onClick={e => that.onDelete(e, event._id)}>
-                        Delete
-                      </Delete>
                     </PriceSelectWrap>
                   </ImagePriceWrap>
-                </Item>;
+                </Item>
+              );
             });
           })()}
           <p>
-            Total: $ {(function() {
+            Total: ${" "}
+            {(function() {
               let total = 0;
               cart.forEach(pair => {
                 total += pair.item.usd;
@@ -86,10 +100,9 @@ export default class Cart extends Component {
             })()}
           </p>
         </ItemsWrap>
-        <Link to="/store">Continue Shopping</Link>
-        <Checkout onClick={() => this.props.history.push("/payment")}>
-          Proceed to checkout
-        </Checkout>
-      </CartWrap>;
+
+        <Checkout onClick={this.onCompletePurchase}>Complete purchase</Checkout>
+      </CartWrap>
+    );
   }
 }
